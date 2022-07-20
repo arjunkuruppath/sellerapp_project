deaf05046b3dfa26e46c4f1c5e9db6ede302198f
Arjun Kuruppath <arjun.k@techversantinfo.com>
2022-07-20 21:22:37 +0530

    email settings modified

--- sellingapp_project/settings.py
+++ sellingapp_project/settings.py
@@ -113,7 +113,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_HOST_USER="arjunkuruppath10@gmail.com"
-EMAIL_HOST_PASSWORD="cojhpsqycmbwucpo"
+EMAIL_HOST_PASSWORD="vpgngpuujwjqmxjz"
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
 
